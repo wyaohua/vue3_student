eb7a49ef30e722f9080d59e815f55ea456856d4c
武耀华 <460615310@qq.com>
2025-10-09 20:30:52 +0800

    setup的学习

---
+++ 01-setup/components/Person语法糖.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="person">
+    <div @click="changeName">{{ name }}</div>
+  </div>
+</template>
+
+<script lang="ts" name="Person" setup>
+import { ref } from 'vue'
+
+
+    let name = ref('张三')
+
+    function changeName() {
+      name.value = '李四'
+    }
+
+
+
+</script>
+
+<style>
+.person {
+  background: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
