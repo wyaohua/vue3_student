d18947ec685b22b390092c6da5da4f74874bcdee
武耀华 <460615310@qq.com>
2025-10-13 01:01:50 +0800

    组件通信

--- src/pages/04_v-model/Father.vue
+++ src/pages/04_v-model/Father.vue
@@ -3,16 +3,7 @@
     <h3>父组件</h3>
     <h4>{{ username }}</h4>
     <h4>{{ password }}</h4>
-    <!-- v-model用在html标签上 -->
-    <!-- <input type="text" v-model="username"> -->
-    <!-- <input type="text" :value="username" @input="username = (<HTMLInputElement>$event.target).value"> -->
 
-    <!-- v-model用在组件标签上 -->
-    <!-- <AtguiguInput v-model="username"/> -->
-    <!-- <AtguiguInput 
-      :modelValue="username" 
-      @update:modelValue="username = $event"
-    /> -->
 
     <!-- 修改modelValue -->
     <AtguiguInput v-model:ming="username" v-model:mima="password"/>
@@ -20,11 +11,11 @@
 </template>
 
 <script setup lang="ts" name="Father">
-	import { ref } from "vue";
-  import AtguiguInput from './AtguiguInput.vue'
-  // 数据
-  let username = ref('zhansgan')
-  let password = ref('123456')
+import { ref } from "vue";
+import AtguiguInput from './AtguiguInput.vue'
+// 数据
+let username = ref('zhansgan')
+let password = ref('123456')
 </script>
 
 <style scoped>
